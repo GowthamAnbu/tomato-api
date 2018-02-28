0539e1979606aad4f282c67c4418f1e4b7a4aa42
Gowtham Anbazhagan <gowthama@tag124.tag.dc>
2018-02-28 13:42:51 +0530

    changed the db url and vscode config to disable eslint

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const rootPath = path.normalize(__dirname +'/../');
 module.exports = {
 	development: {
 		rootPath: rootPath,
-		db: 'mongodb://localhost/tomato',
+		db: 'mongodb://192.168.7.117:27017/tomato',
 		port: process.env.PORT || 3030
 	}
 }
